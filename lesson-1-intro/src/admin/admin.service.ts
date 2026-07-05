28067e7e99aae27a9a59c42a8e3023376f7572e5
PirateKenny01 <shadab.fardin0234@gmail.com>
2026-07-06 01:49:21 +0600

    devmatch project initial start

--- lesson-1-intro/src/admin/admin.service.ts
+++
@@ -1,9 +0,0 @@
-// src/admin/admin.service.ts
-import { Injectable } from '@nestjs/common';
-
-@Injectable()
-export class AdminService 
-{
-  getAdminStats() { return "Fetching stats..."; }
-  // Other methods would go here...
-}
--- lesson-1-intro/src/app.module.ts
+++ lesson-1-intro/src/app.module.ts
@@ -2,10 +2,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AdminModule } from './admin/admin.module'; // Tells NestJS to look inside the admin folder
 
 @Module({
-  imports: [AdminModule], // Registers your admin module globally
   controllers: [AppController],
   providers: [AppService],
 })
