cd23afe51b7a1e16bec36f0976e9c23148a6f765
PirateKenny01 <shadab.fardin0234@gmail.com>
2026-07-06 06:14:49 +0600

    creating decorators in profile.controller.ts

--- devmatch/dist/profiles/profiles.controller.d.ts
+++ devmatch/dist/profiles/profiles.controller.d.ts
@@ -1,2 +1,20 @@
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 export declare class ProfilesController {
+    findAll(location: string): {
+        location: string;
+    }[];
+    findOne(id: string): {
+        id: string;
+    };
+    create(createProfileDto: CreateProfileDto): {
+        name: string;
+        description: string;
+    };
+    Update(id: string, updateProfileDto: UpdateProfileDto): {
+        name: string;
+        description: string;
+        id: string;
+    };
+    remove(id: string): void;
 }
--- devmatch/src/profiles/profiles.controller.ts
+++ devmatch/src/profiles/profiles.controller.ts
@@ -1,4 +1,52 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Query,Param,Post, Body,Put,Delete,HttpCode,HttpStatus } from '@nestjs/common';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Controller('profiles')
-export class ProfilesController {}
+export class ProfilesController 
+{
+  @Get()
+  findAll(
+    @Query('location') location: string) 
+{
+    return [{ location }];
+  }
+
+  @Get(':id')
+    findOne(
+    @Param('id') id: string) 
+    {
+        return { id };
+    }
+
+@Post()
+  create(
+    @Body() createProfileDto: CreateProfileDto) 
+    {
+    return{
+        name: createProfileDto.name,
+        description: createProfileDto.description
+    } ;
+  }
+
+  @Put(':id')
+  Update(
+  @Param('id') id :string,
+  @Body() updateProfileDto: UpdateProfileDto) 
+  {
+    return{
+        id,
+        ...updateProfileDto
+    };
+
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT) // Set the HTTP status code to 200 OK ,but for delete operation, 204 No Content is more appropriate
+  remove(
+    @Param('id') id: string) 
+    {
+        return;
+    }
+
+}
